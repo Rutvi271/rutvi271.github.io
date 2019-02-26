e11cc7827719505e0a17dc6479acc3ee8598747b
rutvi271 <rutvi271@gmail.com>
2019-02-26 17:19:23 +0530

    new theme

--- js/custom.js
+++ js/custom.js
@@ -1,151 +1,216 @@
-// Custom Script
-// Developed by: Samson.Onna
-var customScripts = {
-    profile: function () {
-        // portfolio
-        if ($('.isotopeWrapper').length) {
-            var $container = $('.isotopeWrapper');
-            var $resize = $('.isotopeWrapper').attr('id');
-            // initialize isotope
-            $container.isotope({
-                itemSelector: '.isotopeItem',
-                resizable: false, // disable normal resizing
-                masonry: {
-                    columnWidth: $container.width() / $resize
-                }
-            });
-            $("a[href='#top']").click(function () {
-                $("html, body").animate({ scrollTop: 0 }, "slow");
-                return false;
-            });
-            $('.navbar-inverse').on('click', 'li a', function () {
-                $('.navbar-inverse .in').addClass('collapse').removeClass('in').css('height', '1px');
-            });
-            $('#filter a').click(function () {
-                $('#filter a').removeClass('current');
-                $(this).addClass('current');
-                var selector = $(this).attr('data-filter');
-                $container.isotope({
-                    filter: selector,
-                    animationOptions: {
-                        duration: 1000,
-                        easing: 'easeOutQuart',
-                        queue: false
-                    }
-                });
-                return false;
-            });
-            $(window).smartresize(function () {
-                $container.isotope({
-                    // update columnWidth to a percentage of container width
-                    masonry: {
-                        columnWidth: $container.width() / $resize
-                    }
-                });
-            });
-        }
-    },
-    fancybox: function () {
-        // fancybox
-        $(".fancybox").fancybox();
-    },
-    onePageNav: function () {
+/**	
+	* Template Name: MU Material
+	* Version: 1.0	
+	* Template Scripts
+	* Author: MarkUps
+	* Author URI: http://www.markups.io/
 
-        		if($('#main-nav ul li:first-child').hasClass('active')){
-					$('#main-nav').css('background','none');
-		}
-        $('#mainNav').onePageNav({        
-            currentClass: 'active',
-            changeHash: false,
-            scrollSpeed: 950,
-            scrollThreshold: 0.2,
-            filter: '',
-            easing: 'swing',
-            begin: function () {
-                //I get fired when the animation is starting
-				
-            },
-            end: function () {
-                //I get fired when the animation is ending
-				if(!$('#main-nav ul li:first-child').hasClass('active')){
-					$('.header').addClass('addBg');					
-				}else{
-						$('.header').removeClass('addBg');
-				}
-				
-            },
-            scrollChange: function ($currentListItem) {
-                //I get fired when you enter a section and I pass the list item of the section
-				if(!$('#main-nav ul li:first-child').hasClass('active')){
-					$('.header').addClass('addBg');
-				}else{
-						$('.header').removeClass('addBg');
-				}
-            }
-        });
-    },
-    slider: function () {
-        $('#da-slider').cslider({
-            autoplay: true,
-            bgincrement: 0
-        });
-    },
-    owlSlider: function () {
-        var owl = $("#owl-demo");
-        owl.owlCarousel();
-        // Custom Navigation Events
-        $(".next").click(function () {
-            owl.trigger('owl.next');
-        })
-        $(".prev").click(function () {
-            owl.trigger('owl.prev');
-        })
-    },
-    bannerHeight: function () {
-        var bHeight = $(".banner-container").height();
-        $('#da-slider').height(bHeight);
-        $(window).resize(function () {
-            var bHeight = $(".banner-container").height();
-            $('#da-slider').height(bHeight);
-        });
-    },
-    init: function () {
-        customScripts.onePageNav();
-        customScripts.profile();
-        customScripts.fancybox();
-        customScripts.slider();
-        customScripts.owlSlider();
-        customScripts.bannerHeight();
-    }
-}
-$('document').ready(function () {
-    customScripts.init();
-	$('#diagram-id-1').diagram({ 
-			size: "190",
-			borderWidth: "10",
-			bgFill: "#95a5a6",
-			frFill: "#3498db",
-			textSize: 54,
-			textColor: '#1a1a1a'
-		}); 
-		$('#diagram-id-2').diagram({ 
-			size: "190",
-			borderWidth: "10",
-			bgFill: "#95a5a6",
-			frFill: "#3498db",
-			textSize: 54,
-			textColor: '#333'
-		});
+	Custom JS
+	
+	1. MOBILE MENU
+	2. EXPERIENCE SLIDER (Owl Carousel)
+	3. EDUCATION SLIDER (Owl Carousel)
+	4. PORTFOLIO SLIDER
+	5. COUNTER
+	6. TESTIMONIAL SLIDER (Owl Carousel)	
+	7. MENU SMOOTH SCROLLING
+	8. PRELOADER
+	9. CALL TO ABOUT
+	10. BOTTOM TO UP 
+	11. PARALLAX HEADER
+	12. HIRE ME SCROLL
+	
+	
+**/
 
-		$('#diagram-id-3').diagram({ 
-			size: "190",
-			borderWidth: "10",
-			bgFill: "#95a5a6",
-			frFill: "#3498db",
-			textSize: 54,
-			textColor: '#1a1a1a'
-		});
-		$(window).load(function() { 
-			  $('#filter .current').trigger('click');
-		});
+jQuery(function($){
+
+
+	/* ----------------------------------------------------------- */
+	/*  1. Mobile MENU
+	/* ----------------------------------------------------------- */
+
+    jQuery(".button-collapse").sideNav();
+    
+	/* ----------------------------------------------------------- */
+	/*  2. Experience SLider(Owl Carousel)
+	/* ----------------------------------------------------------- */
+
+	var owl = $("#owl-carousel"); 
+    owl.owlCarousel({
+        items : 4, //4 items above 1024px browser width
+        itemsDesktop : [1024,3], //3 items between 1024px and 901px
+        itemsDesktopSmall : [900,2], // betweem 900px and 601px
+        itemsTablet: [600,1], //1 items between 600 and 0
+        itemsMobile : 1 // itemsMobile disabled - inherit from itemsTablet option
+    });
+    // Slide Navigation
+    jQuery(".next").click(function(){
+        owl.trigger('owl.next');
+    });
+
+    jQuery(".prev").click(function(){
+        owl.trigger('owl.prev');
+    });
+
+
+    /* ----------------------------------------------------------- */
+	/*  3. EDUCATION SLIDER (Owl Carousel)
+	/* ----------------------------------------------------------- */
+
+	var owl1 = $("#owl-carousel1"); 
+	owl1.owlCarousel({
+	    items : 4, //4 items above 1024px browser width
+	    itemsDesktop : [1024,3], //3 items between 1024px and 901px
+	    itemsDesktopSmall : [900,2], // betweem 900px and 601px
+	    itemsTablet: [600,1], //1 items between 600 and 0
+	    itemsMobile : 1 // itemsMobile disabled - inherit from itemsTablet option
+	});
+	// Slide Navigation
+	jQuery(".next1").click(function(){
+	    owl1.trigger('owl.next');
+	});
+
+	jQuery(".prev1").click(function(){
+	    owl1.trigger('owl.prev');
+	});
+	
+    /* ----------------------------------------------------------- */
+	/*  4. PORTFOLIO SLIDER
+	/* ----------------------------------------------------------- */
+
+	jQuery('#portfolio-list').mixItUp();	
+
+	/* ----------------------------------------------------------- */
+	/*  5. COUNTER
+	/* ----------------------------------------------------------- */
+
+	jQuery('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });	  
+
+	/* ----------------------------------------------------------- */
+	/*  6. TESTIMONIAL SLIDER (Owl Carousel)
+	/* ----------------------------------------------------------- */
+
+	var owl2 = $("#owl-carousel2"); 
+    owl2.owlCarousel({
+        items : 2, //4 items above 1024px browser width
+        itemsDesktop : [1024,2], //3 items between 1024px and 901px
+        itemsDesktopSmall : [900,2], // betweem 900px and 601px
+        itemsTablet: [600,1], //1 items between 600 and 0
+        itemsMobile : 1 // itemsMobile disabled - inherit from itemsTablet option
+    });
+
+    // Slide Navigation
+    jQuery(".next2").click(function(){
+        owl2.trigger('owl.next');
+    });
+
+    jQuery(".prev2").click(function(){
+        owl2.trigger('owl.prev');
+    });
+	 
+
+	/* ----------------------------------------------------------- */
+	/*  7. MENU SMOOTH SCROLLING
+	/* ----------------------------------------------------------- */ 
+	
+	//MENU SCROLLING WITH ACTIVE ITEM SELECTED
+
+	// Cache selectors
+	var lastId,
+	topMenu = $(".menu-scroll"),
+	topMenuHeight = topMenu.outerHeight()+13,
+	// All list items
+	menuItems = topMenu.find("a"),
+	// Anchors corresponding to menu items
+	scrollItems = menuItems.map(function(){
+	  var item = $($(this).attr("href"));
+	  if (item.length) { return item; }
+	});
+
+	// Bind click handler to menu items
+	// so we can get a fancy scroll animation
+	menuItems.click(function(e){
+	  var href = $(this).attr("href"),
+	      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+15;
+	  jQuery('html, body').stop().animate({ 
+	      scrollTop: offsetTop
+	  }, 900);
+	  e.preventDefault();
+	});
+
+	// Bind to scroll
+	jQuery(window).scroll(function(){
+	   // Get container scroll position
+	   var fromTop = $(this).scrollTop()+topMenuHeight;
+	   
+	   // Get id of current scroll item
+	   var cur = scrollItems.map(function(){
+	     if ($(this).offset().top < fromTop)
+	       return this;
+	   });
+	   // Get the id of the current element
+	   cur = cur[cur.length-1];
+	   var id = cur && cur.length ? cur[0].id : "";
+	   
+	   if (lastId !== id) {
+	       lastId = id;
+	       // Set/remove active class
+	       menuItems
+	         .parent().removeClass("active")
+	         .end().filter("[href=#"+id+"]").parent().addClass("active");
+	   }           
+	})
+    
+	/* ----------------------------------------------------------- */
+	/*  8. PRELOADER 
+	/* ----------------------------------------------------------- */ 
+
+	jQuery(window).load(function() { // makes sure the whole site is loaded
+      $('.progress').fadeOut(); // will first fade out the loading animation
+      $('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website.
+      $('body').delay(100).css({'overflow':'visible'});
+    })
+	  
+	/* ----------------------------------------------------------- */
+	/* 9. CALL TO ABOUT
+	/* ----------------------------------------------------------- */ 
+	
+	jQuery(".call-to-about").click(function() {
+    jQuery('html,body').animate({
+        scrollTop: $("#about").offset().top},
+        'slow');
+	});
+
+	/* ----------------------------------------------------------- */
+	/* 10. BOTTOM TO UP
+	/* ----------------------------------------------------------- */ 
+
+	jQuery(".up-btn").click(function() {
+    jQuery('html,body').animate({
+        scrollTop: $("#header").offset().top},
+        'slow');
+	});
+
+	/* ----------------------------------------------------------- */
+	/* 11. PARALLAX HEADER
+	/* ----------------------------------------------------------- */ 
+
+	jQuery('.parallax').parallax();
+
+	/* ----------------------------------------------------------- */
+	/* 12. HIRE ME SCROLL
+	/* ----------------------------------------------------------- */ 
+
+	jQuery(".hire-me-btn").click(function(e) {
+		e.preventDefault();
+    jQuery('html,body').animate({
+        scrollTop: $("#footer").offset().top},
+        'slow');
+	});
+
+	
 });
